e7dfba2b2f004d8092fd6f1ff6f2c95d31ec7aa0
Jason Weakley <jason.b.weakley@gmail.com>
2016-01-28 11:30:05 -0600

    adding chinese.js

---
+++ chinese.js
@@ -0,0 +1,20 @@
+
+var Translator = (function (chineseTranslator) {
+	var translationLegend = {"merry":"快活", "christmas":"聖誕", "and":"和", "happy":"快樂", "new":"新", "year":"年"};
+
+
+	return {
+		chineseTranslator.getChinese: function () {
+			return chinese;
+		},
+		chineseTranslator.translateToChinese: function(userInput) {
+			var inputArray = userInput.split(" ");
+			var translatedMessage = "";
+			inputArray.forEach(function(english) {
+				console.log(translationLegend[english]);
+				translatedMessage += translationLegend[english] + "  ";
+			});
+				console.log("translatedMessage", translatedMessage);
+		}
+	}
+}(Translator || {}));
