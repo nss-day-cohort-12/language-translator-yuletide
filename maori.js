1c7d0eb759dde5555b1c384b58fb56b90e18de1a
Dylan Thomas <kdylanthomas@gmail.com>
2016-01-28 10:54:59 -0600

    added html form elements

--- maori.js
+++ maori.js
@@ -1,4 +1,4 @@
-var Translator = function() {
+var Translator = (function(maoriTranslator) {
 	// The lexicon should be a private variable that cannot be accessed 
 	// by the other team member's module except through an accessor (getter) method.
 	var maoriLexicon = {
@@ -9,31 +9,34 @@ var Translator = function() {
 		"new": "hou",
 		"year": "tau"
 	}
-	return {	
-		getMaoriWords: function() {
-			return maoriLexicon;
-		},
-		setMaoriWords: function(a, b) {
-			maoriLexicon[a] = b;
-		},
-		// Each IIFE should expose, in its public interface, a method named 
-		// translateTo{Language} (e.g. translateToSpanish or translateToFinnish) that accepts 
-		// a single argument. That argument's value should be the text entered in by the user.
-		translateToMaori: function(input) {
-			var translatedMessage = "";
-			var inputArray = input.split(" ");
-			console.log("input", inputArray);
-			inputArray.forEach(function(element){
-				if (maoriLexicon.hasOwnProperty(element) === false) {
-					throw "Sorry, I don't know what " + element + " means.";
-				}
-				console.log(maoriLexicon[element]);
-				translatedMessage += maoriLexicon[element] + " ";
-			})
-			// eventually: domElement.innerHTML = `<div>${translatedMessage}</div>`;
-			console.log("translatedMessage:", translatedMessage);
-		}
-	}
-}();
+	
+	maoriTranslator.getMaoriWords = function() {
+		return maoriLexicon;
+	};
+	maoriTranslator.setMaoriWords = function(a, b) {
+		maoriLexicon[a] = b;
+	};
+	// Each IIFE should expose, in its public interface, a method named 
+	// translateTo{Language} (e.g. translateToSpanish or translateToFinnish) that accepts 
+	// a single argument. That argument's value should be the text entered in by the user.
+	maoriTranslator.translateToMaori = function(input) {
+		var translatedContent = document.getElementById("translated-content");
+		var translatedMessage = "";
+		var inputArray = input.split(" ");
+		console.log("input", inputArray);
+		inputArray.forEach(function(element){
+			if (maoriLexicon.hasOwnProperty(element) === false) {
+				throw "Sorry, I don't know what " + element + " means.";
+			}
+			console.log(maoriLexicon[element]);
+			translatedMessage += maoriLexicon[element] + " ";
+		})
+		translatedContent.innerHTML = `<div>${translatedMessage}</div>`;
+		console.log("translatedMessage:", translatedMessage);
+	};
+
+	return maoriTranslator;
+}(Translator || {}));
+
 
 
